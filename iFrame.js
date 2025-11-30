276b0a878b387027ddff557900160bb52cc90ea5
lrgabriel <lrgabriel@users.noreply.github.com>
2025-11-30 17:34:32 +0000

    Add refresh interval code
    From this commit:
    https://github.com/kibotu/iFrame/commit/94a12b53ff18c2f6a3e5cfabbe886756051da230

--- iFrame.js
+++ iFrame.js
@@ -12,8 +12,20 @@ Module.register("iFrame",{
 		defaults: {
 				height:"300px",
 				width:"100%"
+				refreshInterval: 60000
 		},
 
+
+    start: function() {
+        if (this.config.refreshInterval <= 0) {
+            return;
+        }
+
+        var self = this;
+        setInterval(function() {
+                document.getElementById('fancy-iframe').src += '';
+        }, this.config.refreshInterval);
+    },
 
 	// Override dom generator.
 	getDom: function() {
@@ -21,6 +33,7 @@ Module.register("iFrame",{
         div.style = "height:" + this.config.divheight + ";overflow:hidden"
 		var iframe = document.createElement("iframe");
 		iframe.style = "border:0"
+		iframe.id = 'fancy-iframe';
 		iframe.width = this.config.width;
 		iframe.height = this.config.height;
 		iframe.src =  this.config.url;
