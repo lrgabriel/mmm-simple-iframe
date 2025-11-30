15b979bf1ebf07e3180ca1d3baa3b7ac95ea20ec
lrgabriel <lrgabriel@users.noreply.github.com>
2025-11-30 09:44:52 -0800

    Revert "Add refresh interval code"
    This reverts commit 276b0a878b387027ddff557900160bb52cc90ea5.

--- iFrame.js
+++ iFrame.js
@@ -12,20 +12,8 @@ Module.register("iFrame",{
 		defaults: {
 				height:"300px",
 				width:"100%"
-				refreshInterval: 60000
 		},
 
-
-    start: function() {
-        if (this.config.refreshInterval <= 0) {
-            return;
-        }
-
-        var self = this;
-        setInterval(function() {
-                document.getElementById('fancy-iframe').src += '';
-        }, this.config.refreshInterval);
-    },
 
 	// Override dom generator.
 	getDom: function() {
@@ -33,7 +21,6 @@ Module.register("iFrame",{
         div.style = "height:" + this.config.divheight + ";overflow:hidden"
 		var iframe = document.createElement("iframe");
 		iframe.style = "border:0"
-		iframe.id = 'fancy-iframe';
 		iframe.width = this.config.width;
 		iframe.height = this.config.height;
 		iframe.src =  this.config.url;
